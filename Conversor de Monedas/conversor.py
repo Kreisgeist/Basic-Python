78dc16a69323cd1b8fabf5b201431821763c8253
Enrique Lino <venriquelino@gmail.com>
2020-08-28 16:05:17 -0500

    Primer reto

--- Conversor de Monedas/conversor.py
+++ Conversor de Monedas/conversor.py
@@ -5,3 +5,11 @@ dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes $" + dolares + " dólares")
+
+dolares = input("¿Cuantos dolares tienes?: ")
+dolares = float(dolares)
+valor_peso = 0.046
+pesos = dolares / valor_peso
+pesos = round(pesos, 2)
+pesos = str(pesos)
+print("Tienes $" + pesos + " pesos mexicanos")

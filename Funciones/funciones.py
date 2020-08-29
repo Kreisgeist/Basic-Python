eeff6d0f26ac9b3d8850c5b8f6d37708c8c8bcb0
Enrique Lino <venriquelino@gmail.com>
2020-08-29 18:22:58 -0500

    conversor con funciones

--- Funciones/funciones.py
+++ Funciones/funciones.py
@@ -6,20 +6,28 @@
 # imprimir_mensaje()
 # imprimir_mensaje()
 
-def conversacion(mensaje):
-    print("Hola")
-    print("Como estás")
-    print(mensaje)
-    print("Adios")
+# def conversacion(mensaje):
+#     print("Hola")
+#     print("Como estás")
+#     print(mensaje)
+#     print("Adios")
 
 
-opcion = input("Elige una opción (1, 2, 3): ")
+# opcion = input("Elige una opción (1, 2, 3): ")
 
-if opcion == "1":
-    conversacion("Elegiste la opción 1")
-elif opcion == "2":
-    conversacion("Elegiste la opción 2")
-elif opcion == "3":
-    conversacion("Elegiste la opción 3")
-else:
-    print("Escribe una opción correcta")
+# if opcion == "1":
+#     conversacion("Elegiste la opción 1")
+# elif opcion == "2":
+#     conversacion("Elegiste la opción 2")
+# elif opcion == "3":
+#     conversacion("Elegiste la opción 3")
+# else:
+#     print("Escribe una opción correcta")
+
+def suma (a, b):
+    print("Se suman dos números")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
